387032d95f0219196099d85217f12d389e9f41b3
Thành <37612705+becamegod@users.noreply.github.com>
2020-08-29 12:53:19 +0700

    Add Employee Checkin

--- Demo_in_VS2019/EmployeeManagement/Employee.cpp
+++ Demo_in_VS2019/EmployeeManagement/Employee.cpp
@@ -5,6 +5,13 @@ Employee::Employee()
 	Infor anInfor;
 	EInfor = anInfor;
 	Salary = 0;
+	// for testing
+	EInfor.setID(19127109);
+	time_t now = time(0);	tm cdate;
+	localtime_s(&cdate, &now);
+	logday = cdate.tm_mday;
+	logmonth = cdate.tm_mon + 1;
+	logyear = cdate.tm_year + 1900;
 }
 
 void Employee::View_Infor_Empl()
@@ -46,4 +53,35 @@ void Employee::loadEmplData(Account checker)
 		fi.close();
 	}
 	if (fail) cout << "'Load failed!\n";
+}
+
+void Employee::checkin()
+{
+	string file = "Records-";
+	if (logmonth < 10) file += "0";
+	file += to_string(logmonth) + "-" + to_string(logyear) + ".txt";
+	ifstream fi(file);
+	string input, tok;
+	vector<string> save;
+	if (fi)
+	{
+		while (fi.peek() != EOF)
+		{
+			getline(fi, input);
+			istringstream iss(input);
+			getline(iss, tok, ',');
+			if (tok == to_string(EInfor.getID()))
+			{
+				int offset = input.find(",");
+				int i = offset + (logday * 2 - 1);
+				input[i] = '1';				
+			}
+			save.push_back(input);
+		}
+		fi.close();
+		ofstream fo(file);
+		for (int i = 0; i < save.size(); i++) fo << save[i] << endl;
+		fo.close();
+	}
+	else cout << "Can't load record for check-in.\n";
 }
